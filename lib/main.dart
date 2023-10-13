0742e440dd59c2b9e7358ab990f383a9312ccbb7
I6O <i6o@innodata.com>
2023-10-13 16:19:30 +0800

    lesson #11

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(CalculatorApp());
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
-class CalculatorApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Calculator(),
-    );
-  }
-}
-
-class Calculator extends StatefulWidget {
-  @override
-  _CalculatorState createState() => _CalculatorState();
-}
-
-class _CalculatorState extends State<Calculator> {
-  String result = '0';
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculator App'),
+        title: const Text('My First Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+        
       ),
-      body: Column(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Expanded(
-            child: Container(
-              alignment: Alignment.bottomRight,
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                result,
-                style: TextStyle(fontSize: 36.0),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              buildButton('7'),
-              buildButton('8'),
-              buildButton('9'),
-              buildButton('/'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              buildButton('4'),
-              buildButton('5'),
-              buildButton('6'),
-              buildButton('*'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              buildButton('1'),
-              buildButton('2'),
-              buildButton('3'),
-              buildButton('-'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              buildButton('C'),
-              buildButton('0'),
-              buildButton('='),
-              buildButton('+'),
-            ],
+          Text('Hello World!'),
+          OutlinedButton(onPressed: () {}, child: Text('click me')),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text('Inside Container'),
           ),
         ],
       ),
-    );
-  }
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+        child: const Text('click'),
 
-  Widget buildButton(String buttonText) {
-    return Expanded(
-      child: ElevatedButton(
-        // Use ElevatedButton here
-        style: ElevatedButton.styleFrom(
-          primary: Colors.blue, // Set the button color
-          onPrimary: Colors.white, // Set the text color
-        ),
-        onPressed: () => onButtonPressed(buttonText),
-        child: Text(
-          buttonText,
-          style: TextStyle(fontSize: 24.0),
-        ),
+        
       ),
     );
-  }
-
-  void onButtonPressed(String buttonText) {
-    // Implement button press logic here
-    setState(() {
-      if (buttonText == 'C') {
-        result = '0';
-      } else if (buttonText == '=') {
-        result = '3';
-      } else {
-        if (result == '0') {
-          result = buttonText;
-        } else {
-          result += buttonText;
-        }
-      }
-    });
   }
 }
