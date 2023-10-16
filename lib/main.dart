911a84a3cfa15498130326236bee4201ee528c4c
I6O <i6o@innodata.com>
2023-10-16 14:29:28 +0800

    world time project

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,14 @@
+
+import 'package:first_flutter_project/pages/choose_location.dart';
 import 'package:flutter/material.dart';
+import 'package:first_flutter_project/pages/home.dart';
+import 'package:first_flutter_project/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+  initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
     ));
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My First Flutter App'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-        
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('Hello World!'),
-          OutlinedButton(onPressed: () {}, child: Text('click me')),
-          Container(
-            color: Colors.cyan,
-            padding: EdgeInsets.all(30.0),
-            child: Text('Inside Container'),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.red[600],
-        child: const Text('click'),
-
-        
-      ),
-    );
-  }
-}
---
+++ lib/pages/home.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TextButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location')),
+          ],
+        ),
+      ),
+    );
+  }
+}
